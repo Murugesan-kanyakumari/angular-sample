d4d35b401883eb3f1741144037fd408c7b033146
Murugesan-kanyakumari <murugesanir1997@gmail.com>
2022-09-06 05:13:23 +0530

    form initial

--- intern-app/src/app/app.component.ts
+++ intern-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intern-app';
+  registerForm!: FormGroup;
+  submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+        });
+    }
+    get f(): { [key: string]: AbstractControl; }
+{
+  return this.registerForm.controls;
+}
+
+    formSubmit() {
+        this.submitted = true;
+
+        if (this.registerForm.invalid) {
+            return;
+        }
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.registerForm.reset();
+    }
 }
